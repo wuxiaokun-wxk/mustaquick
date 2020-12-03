e250697afc6f6af3196757566e1e72bbc7e8a429
wuxiaokun-wxk <www.18611849514@163.com>
2020-12-03 20:53:51 +0800

    create

---
+++ src/test/java/com/mustaquick/MustaquickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustaquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustaquickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
