0011007df784144daa8f79b4909223aef872d2f3
yuechen.wu <18411849514@163.com>
2021-04-20 22:33:47 +0800

    add Muster.java

--- src/main/java/com/mytest/Muster.java
+++ src/main/java/com/mytest/Muster.java
@@ -1,2 +1,27 @@
-package com.mytest;public class Muster {
+package com.mytest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Muster {
+    public static void main(String args[]){
+        //实例化集合类对象
+        Collection<String> list=new ArrayList<>();
+        //向集合添加数据
+        list.add("wuxiaokun");
+        list.add("wuyuechen");
+        list.add("chenyuanyi");
+        //创建迭代器
+        Iterator iterator=list.iterator();
+        //判断是否有下一个元素
+        while (iterator.hasNext()){
+            //获取集合中的元素
+            String str=(String) iterator.next();
+            System.out.println(str);
+
+        }
+
+    }
 }
+
