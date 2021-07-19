1fe76630f3a25e4206ec5b4b7fb32f552b61ada4
yuechen.wu <18411849514@163.com>
2021-07-19 19:49:51 +0800

    update

--- src/main/java/com/mytest/Jdbc1.java
+++ src/main/java/com/mytest/Jdbc1.java
@@ -1,4 +1,34 @@
 package com.mytest;
 
+
+
+import java.sql.*;
+
 public class Jdbc1 {
+     static final String JDBC_DEIVER="com.mysql.jdbc.Driver";
+     static final String DB_URL="jdbc:mysql://localhost:3306/wuxiaokun";
+     static final String USER="root";
+     static final String PASSWORD="";
+     public static void main(String []args)throws Exception {
+         Class.forName(JDBC_DEIVER);
+         Connection connection =DriverManager.getConnection(DB_URL,USER,PASSWORD);
+         Statement statement=connection.createStatement();
+         String SQL="select id, name, url FROM websites";
+         ResultSet resultSet=statement.executeQuery(SQL);
+         while (resultSet.next()){
+             int id=resultSet.getInt("id");
+             String name=resultSet.getString("name");
+             String url=resultSet.getString("url");
+             System.out.print("ID: " + id);
+             System.out.print(", 站点名称: " + name);
+             System.out.print(", 站点 URL: " + url);
+             System.out.print("\n");
+         }
+         resultSet.close();
+         statement.close();
+         connection.close();
+
+
+
+    }
 }
